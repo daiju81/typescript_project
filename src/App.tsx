d7563799d4a99b985a3d4a434be7422c37d705a4
daiju81 <1d07c.m8k.prv@gmail.com>
2020-06-05 00:29:23 +0900

    hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,42 @@
-import React, { Component } from "react";
-import CharacterList, { Character } from "./CharacterList";
+import React, { FC, useState } from "react";
+import { Button, Card, Statistic } from "semantic-ui-react";
+
 import "./App.css";
 
-class App extends Component {
-  render() {
-    const characters: Character[] = [
-      {
-        id: 1,
-        name: "羽咲 綾乃",
-        age: 16,
-        height: 151
-      },
-      { id: 2, name: "荒垣 なぎさ", age: 18, height: 174 },
-      {
-        id: 3,
-        name: "泉 理子",
-        age: 18
-      }
-    ];
+const App: FC = () => {
+  const [count, setCount] = useState(0);
 
-    return (
-      <div className="container">
-        <header>
-          <h1>キャラクター</h1>
-        </header>
-        <CharacterList school="北小町" characters={characters} />
-      </div>
-    );
-  }
-}
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div className="container">
+      <header>
+        <h1>カウンター</h1>
+      </header>
+      <Card>
+        <Statistic className="number-board">
+          <Statistic.Label>count</Statistic.Label>
+          <Statistic.Value>{count}</Statistic.Value>
+        </Statistic>
+        <Card.Content>
+          <div className="ui two buttons">
+            <Button color="red" onClick={decrement}>
+              -1
+            </Button>
+            <Button color="green" onClick={increment}>
+              +1
+            </Button>
+          </div>
+        </Card.Content>
+      </Card>
+    </div>
+  );
+};
 
 export default App;
